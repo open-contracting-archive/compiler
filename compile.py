cb256c983af71ed65d1cd489183abfbce4776999
Alex Morega <alex@grep.ro>
2014-07-27 14:39:15 +0200

    group by ocid

--- compile.py
+++ compile.py
@@ -1,7 +1,21 @@
-def compile(release_list):
-    [release] = release_list
+from collections import defaultdict
+
+
+def compile(release_doc_list):
+    [release_doc] = release_doc_list
+    release_by_ocid = defaultdict(list)
+
+    for release in release_doc['releases']:
+        ocid = release['releaseMeta']['ocid']
+        release_by_ocid[ocid].append(release)
+
+    records = [
+        dict(ocid=ocid, releases=releases)
+        for (ocid, releases) in sorted(release_by_ocid.items())
+    ]
+
     return {
-        'publisher': release['publisher'],
-        'publishingMeta': release['publishingMeta'],
-        'records': [],
+        'publisher': release_doc['publisher'],
+        'publishingMeta': release_doc['publishingMeta'],
+        'records': records,
     }
--- testsuite/test_compiler.py
+++ testsuite/test_compiler.py
@@ -15,3 +15,100 @@ def test_compile_empty():
     }
 
     assert compile([doc_in]) == doc_out
+
+
+def test_compile_one():
+    doc_in = {
+        "publisher": dict(name='foo', scheme='sch', uid='the_uid', uri='uri'),
+        "publishingMeta": dict(date='2014-07-26'),
+        "releases": [
+            {
+                "releaseMeta": {
+                    "ocid": "foo id",
+                    "releaseID": "",
+                    "releaseTag": "planning",
+                    "locale": "",
+                },
+                "buyer": {
+                    "id": {"name": "", "scheme": "", "uid": "", "uri": ""},
+                },
+                "planning": {},
+                "formation": {
+                    "notice": "",
+                    "itemsToBeProcured": [],
+                    "totalValue": "",
+                    "method": "Open",
+                    "methodJustification": "",
+                    "selectionCriteria": "Lowest Cost",
+                    "selectionDetails": "",
+                    "submissionMethod": "Electronic Auction",
+                    "submissionDetails": "",
+                    "tenderPeriod": "",
+                    "clarificationPeriod": "",
+                    "clarifications": False,
+                    "awardPeriod": "",
+                    "numberOfBidders": 0,
+                    "numberOfBids": 0,
+                    "bidders": [],
+                    "procuringEntity": {
+                        "id": {"name": "", "scheme": "", "uid": "", "uri": ""},
+                    },
+                    "attachments": [],
+                },
+                "award": {},
+                "contract": {},
+                "performance": {},
+            },
+        ],
+    }
+
+    doc_out = {
+        "publisher": dict(name='foo', scheme='sch', uid='the_uid', uri='uri'),
+        "publishingMeta": dict(date='2014-07-26'),
+        "records": [
+            {
+                "ocid": "foo id",
+                "releases": [
+                    {
+                        "releaseMeta": {
+                            "ocid": "foo id",
+                            "releaseID": "",
+                            "releaseTag": "planning",
+                            "locale": "",
+                        },
+                        "buyer": {
+                            "id": {"name": "", "scheme": "", "uid": "", "uri": ""},
+                        },
+                        "planning": {},
+                        "formation": {
+                            "notice": "",
+                            "itemsToBeProcured": [],
+                            "totalValue": "",
+                            "method": "Open",
+                            "methodJustification": "",
+                            "selectionCriteria": "Lowest Cost",
+                            "selectionDetails": "",
+                            "submissionMethod": "Electronic Auction",
+                            "submissionDetails": "",
+                            "tenderPeriod": "",
+                            "clarificationPeriod": "",
+                            "clarifications": False,
+                            "awardPeriod": "",
+                            "numberOfBidders": 0,
+                            "numberOfBids": 0,
+                            "bidders": [],
+                            "procuringEntity": {
+                                "id": {"name": "", "scheme": "", "uid": "", "uri": ""},
+                            },
+                            "attachments": [],
+                        },
+                        "award": {},
+                        "contract": {},
+                        "performance": {},
+                    },
+                ],
+            },
+        ],
+    }
+
+    assert compile([doc_in]) == doc_out
